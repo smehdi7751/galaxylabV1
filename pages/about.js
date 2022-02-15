77f12355c9e9d5410aacd4f51c98783451f7e7b2
smehdi7751 <smehdi7751@gmail.com>
2022-02-15 16:22:40 +0330

    first commit

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function About() {
+  return (
+    <>
+    about us 
+
+    <Link href="/">
+          <a>home</a>
+      </Link>    
+    </>
+  )
+}
